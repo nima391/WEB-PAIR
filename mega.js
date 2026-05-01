ebb67818da1f35b878e24a7bd4475b1e7e658e91
Mr. Nima Streaming <113153450+nima391@users.noreply.github.com>
2026-05-01 14:01:11 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'nimsith391@gmail.com',
+    password: 'NimA391@#$',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
